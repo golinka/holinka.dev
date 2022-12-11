0e902227ab980c8e0518709bcfd53c16092f8551
Artem Golinka <artem.golinka@jii.ng>
2022-12-11 18:23:45 +0100

    feat(app): added the projects list section

---
+++ components/common/Badge.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+interface Props {
+	label: string;
+}
+
+const { label } = defineProps<Props>();
+</script>
+
+<template>
+	<div
+		class="badge text-xs py-1 px-2.5 leading-none whitespace-nowrap align-baseline bg-gray-200 text-gray-700 rounded-lg"
+	>
+		{{ label }}
+	</div>
+</template>
