a5f26ef758ac70b3ce11de92e7d14aed5be61360
Artem Holinka <artem.holinka@gmail.com>
2022-12-22 22:58:08 +0100

    feat(app): added twitter meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 import { resolve } from "path";
 
+const meta = {
+	siteName: "Artem Holinka",
+	title: "Artem Holinka - Frontend Developer",
+	description: `I have over ${new Date().getFullYear() - 2015} years of development experience and currently works as Frontend developer at JIJI. Also I write about JavaScript and other things related to WEB.`,
+	image: "https://holinka.dev/images/avatar.jpg",
+	url: "https://holinka.dev",
+}
+
 export default defineNuxtConfig({
 	ssr: false,
 	app: {
@@ -7,13 +15,17 @@ export default defineNuxtConfig({
 			title: "Artem Holinka - Frontend Developer",
 			viewport: "width=device-width, initial-scale=1",
 			meta: [
-				{ name: "description", content: `I have over ${new Date().getFullYear() - 2015} years of development experience and currently works as Frontend developer at JIJI. Also I write about JavaScript and other things related to WEB.` },
+				{ name: "description", content: meta.description },
 				{ hid: "og-type", property: "og:type", content: "website" },
-				{ hid: "og-site_name", property: "og:type", content: "Artem Holinka" },
-				{ hid: "og-title", property: "og:title", content: "Artem Holinka - Frontend Developer" },
-				{ hid: "og-desc", property: "og:description", content: `I have over ${new Date().getFullYear() - 2015} years of development experience and currently works as Frontend developer at JIJI. Also I write about JavaScript and other things related to WEB.` },
-				{ hid: "og-image", property: "og:image", content: "https://holinka.dev/images/avatar.jpg" },
-				{ hid: "og-url", property: "og:url", content: 'https://holinka.dev' },
+				{ hid: "og-site_name", property: "og:site_name", content: meta.siteName },
+				{ hid: "og-title", property: "og:title", content: meta.title },
+				{ hid: "og-desc", property: "og:description", content: meta.description },
+				{ hid: "og-image", property: "og:image", content: meta.image },
+				{ hid: "og-url", property: "og:url", content: meta.url },
+				{ hid: "twitter-card", property: "twitter:card", content: 'summary_large_image' },
+				{ hid: "twitter-title", property: "twitter:title", content: meta.title },
+				{ hid: "twitter-description", property: "twitter:description", content: meta.description },
+				{ hid: "twitter-image", property: "twitter:image", content: meta.image },
 			],
 			link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
 		},
