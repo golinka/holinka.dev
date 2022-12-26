afbac7070aba4aebddc661143dc0de3cf21539b1
Artem Holinka <artem.holinka@gmail.com>
2022-12-26 22:26:00 +0100

    chore(app): styles refactoring

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -6,13 +6,20 @@ const links = ref([
 </script>
 
 <template>
-	<footer class="footer flex items-center justify-between text-sm border-t border-t-gray-200 py-8">
+	<footer class="footer flex items-center justify-between text-sm border-t border-t-gray-200 text-gray-500 py-8">
 		<div class="footer__copyright">
 			© {{ new Date().getFullYear() }} <span class="ml-1">Made with ❤️ by Artem Holinka</span>
 		</div>
 		<div class="footer__social">
-			<NuxtLink v-for="(link, index) in links" :key="index" :to="link.url" target="_blank" rel="noindex" class="mr-3 last:mr-0">
-				<Icon :name="link.icon" size="24" class="color-gray-300" />
+			<NuxtLink
+				v-for="(link, index) in links"
+				:key="index"
+				:to="link.url"
+				target="_blank"
+				rel="noindex"
+				class="mr-3 last:mr-0"
+			>
+				<Icon :name="link.icon" size="24" />
 			</NuxtLink>
 		</div>
 	</footer>
