c7419bb826091387187c117e1052c7a9097d2f69
Artem Holinka <artem.holinka@gmail.com>
2023-02-26 18:50:00 +0100

    chore(app): refactoring

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ArticleQueryContent } from "@/types/article";
+import { ArticleQueryContent } from "~~/types/article";
 import ShortAboutMe from "~~/components/pages/home/ShortAboutMe.vue";
 import LatestArticles from "~~/components/pages/home/LatestArticles.vue";
 import ProjectsList from "~~/components/pages/home/ProjectsList.vue";
@@ -12,7 +12,7 @@ const { data: articles } = await useAsyncData("blog", () => queryContent<Article
 	<NuxtLayout name="default">
 		<div class="home-page">
 			<ShortAboutMe class="mb-10"></ShortAboutMe>
-			<LatestArticles v-if="articles" :articles="articles" class="mb-10"></LatestArticles>
+			<LatestArticles v-if="articles && articles.length" :articles="articles" class="mb-10"></LatestArticles>
 			<ProjectsList v-if="false" class="mb-10"></ProjectsList>
 			<LatestExperience v-if="false" class="mb-10"></LatestExperience>
 		</div>
