ede3d74d8dacba4227998a763cde77904ad1ec0a
Artem Holinka <artem.holinka@gmail.com>
2023-02-28 23:40:09 +0100

    feat(app): added seo tags for the article page

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -2,15 +2,41 @@
 	<NuxtLayout name="default">
 		<ContentDoc>
 			<template #empty>
-				<p>No content found.</p>
+				<article>
+					<p>No content found.</p>
+				</article>
 			</template>
 
 			<template #default="{ doc }">
-				<article>
-					<h1>{{ doc.title }}</h1>
+				<article class="prose">
+					<ProseH1 id="Heading">{{ doc.title }}</ProseH1>
+					<ProseImg v-if="doc.image" :src="doc.image" :alt="doc.title"></ProseImg>
 					<ContentRenderer :value="doc" />
 				</article>
 			</template>
 		</ContentDoc>
 	</NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import { ArticleQueryContent } from "~~/types/article";
+
+const route = useRoute();
+
+const { data: article } = await useAsyncData("blog", () => queryContent<ArticleQueryContent>(route.path).findOne());
+
+if (article.value) {
+	useServerSeoMeta({
+		title: article.value.title,
+		ogType: "article",
+		ogTitle: article.value.title,
+		description: article.value.description,
+		ogDescription: article.value.description,
+		ogImage: article.value.image,
+		twitterCard: "summary_large_image",
+		twitterTitle: article.value.title,
+		twitterDescription: article.value.description,
+		twitterImage: article.value.image,
+	});
+}
+</script>
