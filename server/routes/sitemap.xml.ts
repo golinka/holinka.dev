96d2fd8797e986b874c6a38837cae88cfd3827ae
Artem Holinka <artem.holinka@gmail.com>
2023-03-15 11:41:07 +0100

    chore(app): sitemap refactoring

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,11 +1,10 @@
 import { serverQueryContent } from "#content/server";
 import { SitemapStream, streamToPromise } from "sitemap";
 
-const routes = ["/", "/blog", "/about"];
+const basicRoutes = [{ _path: "/", changefreq: "daily" }, { _path: "/blog", changefreq: "daily" }, { _path: "/about" }];
 
 export default defineEventHandler(async (event) => {
 	// Fetch all documents
-	const basicRoutes = routes.map((i) => ({ _path: i }));
 	const docsRoutes = await serverQueryContent(event).sort({ date: -1 }).where({ _partial: false }).find();
 
 	const sitemap = new SitemapStream({
@@ -15,7 +14,7 @@ export default defineEventHandler(async (event) => {
 	for (const route of [...docsRoutes, ...basicRoutes]) {
 		sitemap.write({
 			url: route._path,
-			changefreq: "monthly",
+			changefreq: route.changefreq || "monthly",
 		});
 	}
 
