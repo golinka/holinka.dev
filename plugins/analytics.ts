a24061fb89631c6e0670a62789f9cb268a7a48b0
Artem Holinka <artem.holinka@gmail.com>
2023-03-22 13:01:00 +0100

    chore(app): refactoring

--- plugins/analytics.ts
+++ plugins/analytics.ts
@@ -2,13 +2,11 @@ import VueGtag from "vue-gtag-next";
 
 export default defineNuxtPlugin((nuxtApp) => {
 	const config = useRuntimeConfig();
-	console.warn('nuxtApp >>>', nuxtApp)
 	if (config.public.isProd) {
 		nuxtApp.vueApp.use(VueGtag, {
 			property: {
 				id: config.public.googleAnalyticsId,
 			},
 		});
-		nuxtApp.$gtag.config()
 	}
 });
