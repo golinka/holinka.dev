86aacc7cc1bcb029ad99893d0000c04d2e01da75
Artem Holinka <artem.holinka@gmail.com>
2023-04-03 14:11:49 +0200

    feat(app): added the 404 page

---
+++ error.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+interface IProps {
+	error: { url: string; statusCode: string; statusMessage: string; message: string; description: string; data: any };
+}
+const { error } = defineProps<IProps>();
+</script>
+
+<template>
+	<div class="error-page w-full h-screen flex items-center justify-center bg-gradient-to-r from-slate-100 via-purple-100 to-slate-100 background-animate">
+		<div class="error-page-block">
+			<div class="error-page-block__icon text-7xl flex font-extrabold justify-center mb-2">
+				{{ error.statusCode || "Error page" }}
+			</div>
+			<h1 class="error-page-block__title text-3xl font-bold text-center mb-6">Ooops...</h1>
+			<div class="error-page-block__message text-center">{{ error.message }}</div>
+		</div>
+	</div>
+</template>
