e3b64e6806d1833a4d1437f1428612bc81135582
Artem Holinka <artem.holinka@gmail.com>
2024-08-03 15:51:22 +0200

    feat(about): updated text on the page

--- pages/about.vue
+++ pages/about.vue
@@ -22,30 +22,27 @@ useServerSeoMeta({
 		<article class="container">
 			<ProseH2 id="about">👋 About me</ProseH2>
 			<ProseP>
-				I'm a Senior Frontend Developer at Jiji, a leading online marketplace in Africa with over 7 million monthly
-				visitors. With over {{ new Date().getFullYear() - 2015 }} years of experience in web and mobile development,
-				I’ve refined my skills in startups, outsourcing, and product companies across multiple sectors.
+				I'm a Senior Frontend Developer at Jiji, a leading online marketplace in Africa with over 7 million monthly visitors.
+				I have over {{ new Date().getFullYear() - 2015 }} years of experience in web and mobile development.
 			</ProseP>
 			<ProseP>
-				I am passionate about creating user-centered products that are designed with attention to performance and
-				security but also aligned with business goals.
+				It includes working in startups, outsourcing, and product companies across multiple sectors.
+				I'm passionate about creating useful products for customers that are designed with attention to performance and security but also aligned with business goals.
 			</ProseP>
 			<ProseP>
-				I also had the opportunity to participate in developer competitions and hackathons, both as a participant and as
-				a judge, which further expanded my technical experience and problem-solving skills.
+				Additionally, I had the opportunity to participate in developer competitions and hackathons,
+				both as a participant and as a judge, which further expanded my technical experience and problem-solving skills.
 			</ProseP>
 
 			<ProseH2 id="story">🎯 Personal Interests</ProseH2>
 			<ProseP>
-				When I’m not coding, you’ll find me traveling 🌎, playing football ⚽️ and table tennis 🏓. I’m lucky that my
-				passion for web development aligns with my profession, and I love what I do 🥳.
+				When I'm not coding, you'll find me traveling 🌎, playing football ⚽️ and table tennis 🏓.
 			</ProseP>
 
 			<ProseH2 id="story">💬 Let's talk</ProseH2>
 			<ProseP>
-				Besides, I am always willing to help others with career development, overcoming growth challenges, and
-				facilitating smooth onboarding experiences. If you need advice or collaboration, feel free to
-				<ProseA href="https://meander.so/m/artem-holinka" rel="nofollow noreferrer" blank>reach out</ProseA>!
+				Besides, I am always willing to help others with career development, overcoming growth challenges, and facilitating smooth onboarding experiences.
+				If you need advice or collaboration, feel free to <ProseA href="https://meander.so/m/artem-holinka" rel="nofollow noreferrer" blank>reach out</ProseA>!
 			</ProseP>
 
 			<ProseH2 id="find-or-contact">Find or contact me</ProseH2>
